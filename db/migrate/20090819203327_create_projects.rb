64a8ed7bda7c8ff14ebcc5c14c4a532150b78f22
Pavel Zaitsev <pavel@arslogic.ca>
2009-08-19 16:38:53 -0400

    added nifty generators...

---
+++ db/migrate/20090819203327_create_projects.rb
@@ -0,0 +1,16 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.string :name
+      t.integer :owner_id
+      t.integer :icon_id
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
