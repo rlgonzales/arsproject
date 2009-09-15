afc147bd5a729b0af2cbf17a45a697441870ff8b
Pavel Zaitsev <pavel@arslogic.ca>
2009-09-15 06:18:41 -0400

    Adding elements to UI, hiding draggable bar and adding a close element.

--- public/javascripts/ap.js
+++ public/javascripts/ap.js
@@ -6,13 +6,14 @@ var offset = 0;
 var zi = 0;
 
 // here we load a div of some sort. When its done loading we run event handler
-var ap_showPartial = function(url,container,focus_element){
+var ap_showPartial = function(url,container,focus_element,submit_element){
   var ct = container;
   if(container == null)
     ct = $('container');
   
+  var to_hide = 'ap_partial_'+zi.toString();
   var el = new Element('div',{
-    'id':'ap_partial_'+zi.toString(),
+    'id':to_hide,
     'styles':{
       'position':'absolute',
       'display':'block',
@@ -25,48 +26,98 @@ var ap_showPartial = function(url,container,focus_element){
   el.setStyle('visibility','hidden');
   el.inject(ct,'top');
   
-  el.set('load',{onSuccess: function(resp){
-    // drag handle bar
-    var drag_el = new Element('div',{
-        'styles':{
-          'height':'40px',
-          'width':'100px',
-          'background-color':'gray',
-          'visibility':'visible'
-        }
-      });
+        el.set('load',{onSuccess: function(resp){
+          // drag handle bar
+          var drag_el = new Element('div',{
+              'styles':{
+                position: 'absolute',
+                'height':'40px',
+                'width':'100px',
+                'background':'transparent',
+                'visibility':'visible'
+              }
+            });
 
-    var dg = new Drag(el,{'handle':drag_el});
+          var close_el = new Element('div',{
+            styles:{
+              height:'32px',
+              width:'32px',
+              'background-color':'red',
+              'float':'right'
+            }
+          });
+          
 
-    drag_el.inject(el,'top');
+          // hide the dialog
+          close_el.inject(el,'top');
+          close_el.addEvent('click',function(ev){
+            ev.stop();
+            el.setStyle('display','none');
+          }.bindWithEvent(this,[el]));
 
-    var sz = el.getSize();
-    el.setStyles({
-        left: 500,
-        top: -sz.y
-    });
-    
-    el.setStyle('visibility','visible');
-    var wsz = window.getSize();
+          var dg = new Drag(el,{'handle':drag_el});
 
-    el.get('tween').start('top',[-sz.y,50+offset]).chain(function(){
-      if(focus_element) $(focus_element).focus();
-     });
+          var esz = el.getSize();
+          drag_el.setStyle('width',esz.x-32);
 
-    offset += 30;
-    zi ++;
-    ap_hookElementsIn();
-  }});
-  
+          drag_el.inject(el,'top');
+
+          var sz = el.getSize();
+          el.setStyles({
+              left: 500,
+              top: -sz.y
+          });
+          
+          el.setStyle('visibility','visible');
+          var wsz = window.getSize();
+
+          el.get('tween').start('top',[-sz.y,50+offset]).chain(function(){
+            if(focus_element) $(focus_element).focus();
+           });
+
+          var sel = $(submit_element);
+          if( sel ){
+            sel.set( 'to_hide', to_hide );
+          };
+        
+          offset += 30;
+          zi ++;
+          ap_hookElementsIn();
+        }});
+        
   el.load(url);
 };
 
-var ap_submitAndClosePartial = function(event,url,partial_to_close){
+// Sumbits the form hides the element... should update something?
+var ap_submitAndClosePartial = function(event,url,partial_to_close,submit_element,callback)
+{
     event.stop();
-    alert('doot!');
+    if(callback)
+    {
+      callback($(submit_element).form);
+    }
+
+    $(submit_element).form.send();
+    $($(submit_element).get('to_hide')).setStyle('display','none');
+    
 };
 
-var ap_hookElementsIn = function(){
+var ap_copyMilestoneSubject = function(form)
+{
+    
+    var newdiv = new Element('div',{
+                              'styles':{
+                                'display':'block'
+                              }
+                            });
+    newdiv.set('text',form.milestone_name.value);
+    newdiv.inject($('ap_milestones_list'),'top');
+};
+
+
+// Hooks up elements to their function handelers
+var ap_hookElementsIn = function()
+{
   var hk = new Hash(hooks);
   hk.each(function(val,key){
     if($(key))
@@ -76,11 +127,36 @@ var ap_hookElementsIn = function(){
 
 hooks = 
 {
-  'ap_add_new_milestone':[ 'click', ap_showPartial.bind( null, [ '/milestones/new/?layout=naked' , 'container' ,'milestone_name'] ) ],
-  'milestone_submit':['click',ap_submitAndClosePartial.bindWithEvent(null, ['/milestones',null] ) ]
+  'ap_add_new_milestone':[
+                        'click',
+                        ap_showPartial.bind(
+                            null,
+                            [
+                              '/milestones/new/?layout=naked',
+                              'container',
+                              'milestone_name',
+                              'milestone_submit'
+                            ]
+                            
+                         )
+                        ],
+
+  'milestone_submit':[
+                        'click',
+                        ap_submitAndClosePartial.bindWithEvent(
+                                                null,
+                                                [
+                                                  '/milestones',
+                                                  null,
+                                                  'milestone_submit', 
+                                                  ap_copyMilestoneSubject
+                                                ]
+                                              )
+                     ]
 };
 
 window.addEvent('domready',function(){
   ap_hookElementsIn();
   DD_roundies.addRule('#ap_new_milestone','15px',true);
 });
+
