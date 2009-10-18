5d7ba9ae561951b2a2fc25b23adebbbc4e60cf8b
Pavel Zaitsev <pavel@arslogic.ca>
2009-10-17 23:31:24 -0400

    Now updates on creating of each project go to each of person's friends. Yay.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,15 @@ class ProjectsController < ApplicationController
   make_resourceful do
     actions :all
     after :create do
+      
+      current_user.friends.each do |f|
+        # LoGG!
+        u  = Update.new
+        u.cache = '<a href="' + user_path(current_user) + "\">"+current_user.full_name+"</a> created new project '<a href=\""+ project_path( current_object ) + "\">"+ current_object.name+"</a>'"
+        u.owner_id = f.id
+        u.save
+      end
+
       # LoGG!
       u  = Update.new
       u.cache = "Added new project '<a href=\""+ project_path( current_object ) + "\">"+ current_object.name+"</a>'"
